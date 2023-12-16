b3fcdcc07f308995e2438361a2813a94acee0042
Ephrem Mamo <ephremmamo555@gmail.com>
2023-12-16 09:01:03 -0900

    smooth scrolls

--- src/js/final.js
+++ src/js/final.js
@@ -1,6 +1,7 @@
 
 
 // Mobile Responsive Nav Bar
+
 const menu = document.getElementById("menu");
 menu.addEventListener("click",function(){
     document.querySelector(".header__sidebar").style.display = "flex";
@@ -15,12 +16,44 @@ close.addEventListener("click",function(){
 })
 
 
-// Change Header whilw Scrolling
+// Change Header Color while Scrolling because originaly opacity was low
 
-window.addEventListener("scroll", function(){
+document.addEventListener("scroll", function(){
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+
+// Smooth scroll to about page
+
+function smoothScroll() {
+    var targetElement = document.getElementById("about");
+
+    if (targetElement) {
+        // Scroll to the target element smoothly
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.querySelector("li.close").onclick = smoothScroll();
+
+
+
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+        else{
+            entry.target.classList.remove("show");
+        };
+    });
+});
+const hiddenElements = document.querySelectorAll("section");
+hiddenElements.forEach((el)=>observer.observe(el));
 
 
 
